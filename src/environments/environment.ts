3779db46b4e0a2dcec9aee9abc687582ede6275a
Daniel Carvalho <daniel.devop@gmail.com>
2021-08-31 07:26:37 -0300

    Configuração do firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJ0jeWD6PYW80OOUUrUajLT84ViGfgtkA",
+    authDomain: "requisicoes-app-c8e31.firebaseapp.com",
+    projectId: "requisicoes-app-c8e31",
+    storageBucket: "requisicoes-app-c8e31.appspot.com",
+    messagingSenderId: "7012992181",
+    appId: "1:7012992181:web:c247030c21995fad312afc",
+    measurementId: "G-VSX5QKBQXT"
+  }
 };
 
 /*
