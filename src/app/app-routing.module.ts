6ca525b7a1bc70f36be8c80c620f2ca08a1a7a94
Daniel Carvalho <daniel.devop@gmail.com>
2021-09-14 18:27:58 -0300

    Rota para painel com AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/public/login/login.component';
+import { AuthguardService } from './services/authguard.service';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent}
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
+  {
+    path: 'admin/painel',
+    loadChildren: ()=> import('./components/admin/painel/painel.module')
+      .then(m=>m.PainelModule),
+    canActivate: [AuthguardService]
+  },
 ];
 
 @NgModule({
