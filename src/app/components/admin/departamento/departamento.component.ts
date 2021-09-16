7f442db367860ca25dd2a15f8bb6b7289910f46a
Daniel Carvalho <daniel.devop@gmail.com>
2021-09-16 17:21:53 -0300

    Lógica de manipulação do departamento

--- src/app/components/admin/departamento/departamento.component.ts
+++ src/app/components/admin/departamento/departamento.component.ts
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { DepartamentoService } from './../../../services/departamento.service';
 import { Departamento } from './../../../models/departamento.model';
 import { Component, OnInit } from '@angular/core';
@@ -11,8 +12,8 @@ import { Observable } from 'rxjs';
 })
 export class DepartamentoComponent implements OnInit {
   departamentos$: Observable<Departamento[]>;
-  edit: boolean = false;
-  displayDialogDepartamento: boolean = false;
+  edit: boolean;
+  displayDialogDepartamento: boolean;
   form: FormGroup;
 
   constructor( private departamentoService: DepartamentoService, private fb: FormBuilder ) {
@@ -26,7 +27,59 @@ export class DepartamentoComponent implements OnInit {
     })
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.departamentos$ = this.departamentoService.list();
+    this.configForm();
   }
 
+  add() {
+    this.form.reset();
+    this.edit= false;
+    this.displayDialogDepartamento = true;
+  }
+
+  selecionaDepartamento(depto: Departamento){
+    this.edit = true;
+    this.displayDialogDepartamento=true;
+    this.form.setValue(depto);
+  }
+
+  save(){
+    this.departamentoService.createOrUpdate(this.form.value)
+      .then(()=>{
+        this.displayDialogDepartamento=false;
+        Swal.fire(
+          `Departamento ${!this.edit?'Salvo':'Atualizado'} com sucesso`,
+          '','success'
+        );
+      })
+      .catch((erro)=>{
+        this.displayDialogDepartamento=false;
+        Swal.fire(
+          `Erro ao ${!this.edit?'Salvo':'Atualizado'} o departamento.`,
+          `Detalhes: ${erro}`, 'error'
+        );
+      });
+    this.form.reset();
+  }
+
+  delete(depto: Departamento){
+    Swal.fire({
+      title: 'Confirma a exclusão do departamento?',
+      text: '',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    })
+      .then((result)=>{
+        if (result.value){
+          this.departamentoService.delete(depto.id)
+            .then(()=>{
+              Swal.fire('Departamento excluído com sucesso!',
+              '','success');
+            })
+        }
+      })
+  }
 }
