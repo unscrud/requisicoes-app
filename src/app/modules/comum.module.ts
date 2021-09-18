8e16a6d68e788c8a10422614045a6415a2a365ff
Daniel Carvalho <daniel.devop@gmail.com>
2021-09-18 12:14:35 -0300

    Modulo comum

---
+++ src/app/modules/comum.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PrimeNGModule } from './primeng.module';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimeNGModule
+  ]
+})
+export class ComumModule { }
