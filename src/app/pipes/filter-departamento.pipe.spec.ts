50ab2b155be5456f2ab801c117d9866861e63630
Daniel Carvalho <daniel.devop@gmail.com>
2021-09-23 22:24:33 -0300

    Filtro departamento

---
+++ src/app/pipes/filter-departamento.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterDepartamentoPipe } from './filter-departamento.pipe';
+
+describe('FilterDepartamentoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterDepartamentoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/filter-departamento.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterDepartamentoPipe implements PipeTransform {
+
+  transform(value: any, filtro: any): any {
+    if (filtro == 'TODOS') return value;
+    if (value){
+      return value.filter(elem =>(elem.departamento.nome === filtro))
+    }
+  }
+
+}
