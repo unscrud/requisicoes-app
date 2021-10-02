82ee24b83a07d420d40dab3b665ecdfe5270f0a3
Daniel Carvalho <daniel.devop@gmail.com>
2021-10-02 13:05:11 -0300

    Remoção do modelo

--- src/app/models/movimentacao.model.ts
+++
@@ -1,9 +0,0 @@
-import { Funcionario } from './funcionario.model';
-import { Model } from "../core/model";
-
-export class Movimentacao extends Model{
-  funcionario: Funcionario;
-  dataHora: any;
-  status: string;
-  descricao: string;
-}
