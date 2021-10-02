9dfa5cf047dec64335426cce7bd04c6f42b6d4c3
Daniel Carvalho <daniel.devop@gmail.com>
2021-10-02 13:05:35 -0300

    Classe movimentação

--- src/app/models/requisicao.model.ts
+++ src/app/models/requisicao.model.ts
@@ -1,4 +1,3 @@
-import { Movimentacao } from './movimentacao.model';
 import { Departamento } from './departamento.model';
 import { Model } from "../core/model";
 import { Funcionario } from "./funcionario.model";
@@ -11,4 +10,11 @@ export class Requisicao extends Model {
   status: string;
   destino: Departamento;
   movimentacoes: Movimentacao[];
+}
+
+export class Movimentacao extends Model{
+  funcionario: Funcionario;
+  dataHora: any;
+  status: string;
+  descricao: string;
 }
