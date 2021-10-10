e1662ce0b51afbf335875267b414f2b79c8ab7a7
Daniel Carvalho <daniel.devop@gmail.com>
2021-10-10 18:47:36 -0300

    Lista component

---
+++ src/app/components/admin/movimentacao/lista/lista.component.ts
@@ -0,0 +1,105 @@
+import Swal from 'sweetalert2';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Funcionario } from 'src/app/models/funcionario.model';
+import { Movimentacao, Requisicao } from 'src/app/models/requisicao.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RequisicaoService } from 'src/app/services/requisicao.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+
+export class ListaComponent implements OnInit {
+
+  @Input() movimentacoes: Movimentacao[];
+  @Input() requisicaoSelecionada: Requisicao;
+  @Input() displayDialogMovimentacoes: boolean;
+  @Input() funcionarioLogado: Funcionario;
+  @Output() displayChange = new EventEmitter();
+
+  listaStatus: string[];
+  displayDialogMovimentacao: boolean;
+  form: FormGroup;
+  edit: boolean;
+  indexMovimentacoes: number;
+
+  constructor(
+    private requisicaoService: RequisicaoService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.configForm();
+    this.carregaStatus();
+  }
+
+  carregaStatus() {
+    this.listaStatus = ['Aberto','Pendente','Processando','Não autorizada','Finalizado'];
+  }
+
+  configForm() {
+    this.form = this.fb.group({
+      funcionario: new FormControl('',Validators.required),
+      status: new FormControl('',Validators.required),
+      descricao: new FormControl('',Validators.required),
+      dataHora: new FormControl('')
+    });
+  }
+
+  selecionaMovimento(mov: Movimentacao, index: number){
+    this.edit = true;
+    this.displayDialogMovimentacao = true;
+    this.form.setValue(mov);
+    this.indexMovimentacoes = index;
+  }
+
+  onClose(){
+    this.displayChange.emit(false);
+  }
+
+  update(){
+    this.movimentacoes[this.indexMovimentacoes] = this.form.value;
+    this.requisicaoSelecionada.movimentacoes = this.movimentacoes;
+    this.requisicaoSelecionada.status = this.form.controls['status'].value;
+    this.requisicaoSelecionada.ultimaAtualizacao = new Date();
+    this.requisicaoService.createOrUpdate(this.requisicaoSelecionada)
+      .then(()=>{
+        this.displayDialogMovimentacao = false;
+        Swal.fire(`Movimentação ${!this.edit?'salva':'atualizada'} com sucesso.`,'','success')
+      })
+      .catch((erro)=>{
+        this.displayDialogMovimentacao = true;
+        Swal.fire(`Erro ao ${!this.edit?'salvar':'atualizar'} a movimentação.`,``,'error')
+      });
+    this.form.reset();
+  }
+
+  remove(array, element){
+    return array.filter( el => el !== element );
+  }
+
+  delete(mov: Movimentacao){
+    const movs = this.remove( this.movimentacoes, mov);
+    Swal.fire({
+      title: 'Confirma a exclusão da Movimentação?',
+      text: "",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    }).then((result)=>{
+      if (result.value){
+        this.requisicaoSelecionada.movimentacoes = movs;
+        this.requisicaoService.createOrUpdate(this.requisicaoSelecionada)
+          .then(()=>{
+            Swal.fire('Movimentação excluída com sucesso!','','success');
+            this.movimentacoes = movs;
+          })
+      }
+    })
+  }
+
+
+}
