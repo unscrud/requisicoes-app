8646e75e3b344aca0ca18948737cc3d9b3ea9508
Daniel Carvalho <daniel.devop@gmail.com>
2021-10-16 06:42:21 -0300

    Removendo import não utilizado

--- src/app/components/admin/movimentacao/lista/lista.module.ts
+++ src/app/components/admin/movimentacao/lista/lista.module.ts
@@ -1,15 +1,10 @@
 import { NgSelectModule } from '@ng-select/ng-select';
 import { ComumModule } from 'src/app/modules/comum.module';
 import { NgModule } from '@angular/core';
-
 import { ListaRoutingModule } from './lista-routing.module';
-import { ListaComponent } from './lista.component';
-
 
 @NgModule({
-  declarations: [
-    ListaComponent
-  ],
+  declarations: [],
   imports: [
     ComumModule,
     ListaRoutingModule,
