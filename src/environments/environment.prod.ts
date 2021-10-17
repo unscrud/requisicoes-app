164fec02c6ada7174875efb557452c8a9fb5207f
Daniel Carvalho <daniel.devop@gmail.com>
2021-10-17 16:35:10 -0300

    Fim do projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDJ0jeWD6PYW80OOUUrUajLT84ViGfgtkA",
-    authDomain: "requisicoes-app-c8e31.firebaseapp.com",
-    projectId: "requisicoes-app-c8e31",
-    storageBucket: "requisicoes-app-c8e31.appspot.com",
-    messagingSenderId: "7012992181",
-    appId: "1:7012992181:web:c247030c21995fad312afc",
-    measurementId: "G-VSX5QKBQXT"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDJ0jeWD6PYW80OOUUrUajLT84ViGfgtkA",
-    authDomain: "requisicoes-app-c8e31.firebaseapp.com",
-    projectId: "requisicoes-app-c8e31",
-    storageBucket: "requisicoes-app-c8e31.appspot.com",
-    messagingSenderId: "7012992181",
-    appId: "1:7012992181:web:c247030c21995fad312afc",
-    measurementId: "G-VSX5QKBQXT"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
